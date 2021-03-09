1f96920ac7f3f1f07e724fa6d38561322c1c1dcd
하영 <@LAPTOP-0AS9OEBG>
2021-03-09 15:34:22 +0900

    반복 예제

---
+++ day06/src/loop/Test01.java
@@ -0,0 +1,23 @@
+package loop;
+
+import java.lang.*;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//Q : 우리모두 화이팅! 이란 글자를 10번 화면에 출력하세요
+		//[1] 직접 출력을 10번 수행
+		//[2] 한 번만 작성하고 10번 반복
+		
+		System.out.println("우리모두 화이팅!");
+		System.out.println("우리모두 화이팅!");
+		System.out.println("우리모두 화이팅!");
+		System.out.println("우리모두 화이팅!");
+		System.out.println("우리모두 화이팅!");
+		System.out.println("우리모두 화이팅!");
+		System.out.println("우리모두 화이팅!");
+		System.out.println("우리모두 화이팅!");
+		System.out.println("우리모두 화이팅!");
+		System.out.println("우리모두 화이팅!");
+		
+	}
+}
---
+++ day06/src/loop/Test02.java
@@ -0,0 +1,20 @@
+package loop;
+
+import java.lang.*;
+
+public class Test02 {
+	public static void main(String[] args) {
+		
+//		for(10번) {
+//		for(1부터 10까지 1씩증가) {
+//		for(선언부 ; 조건부 ; 증감부) {
+		for(int i = 1 ; i <= 10 ; i = i + 1) {
+			System.out.println("우리모두 화이팅!");
+		}
+		
+		for(int i = 0 ; i < 10 ; i = i + 1) {
+			System.out.println("테스트");
+		}
+		
+	}
+}
