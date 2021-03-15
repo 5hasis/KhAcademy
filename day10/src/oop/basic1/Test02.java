9393cf52467f023145d3f1c01033899c7046920e
하영 <@LAPTOP-0AS9OEBG>
2021-03-15 10:36:19 +0900

    day10 업로드

---
+++ day10/src/oop/basic1/Test02.java
@@ -0,0 +1,13 @@
+package oop.basic1;
+
+public class Test02 {
+	public static void main(String[] args) {
+		//미리 만들어 놓은 클래스(Message)를 이용해 객체를 생성
+		Message a = new Message();
+		Message b = new Message();
+		Message c = new Message();
+		Message d = new Message();
+
+	}
+
+}
