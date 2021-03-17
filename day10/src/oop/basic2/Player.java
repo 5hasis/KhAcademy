0bfc11c973661d639b36caad2a9e6be8d9a25530
하영 <@LAPTOP-0AS9OEBG>
2021-03-17 11:47:47 +0900

    올림픽 선수 문제(객체)

---
+++ day10/src/oop/basic2/Player.java
@@ -0,0 +1,9 @@
+package oop.basic2;
+
+public class data {
+	String name;
+	String sport;
+	int gold;
+	int silver;
+	int bronze;
+}
---
+++ day10/src/oop/basic2/Test01.java
@@ -0,0 +1,16 @@
+package oop.basic2;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//
+		data a = new data();
+		a.name = "김연아";
+		a.sport = "피겨스케이팅";
+		a.gold = 2;
+		a.bronze = 1;
+		
+		System.out.println(a.name);
+
+	}
+
+}
