49e75188a9ebf1cb7e480574704d046a383230ca
하영 <@LAPTOP-0AS9OEBG>
2021-03-17 11:48:26 +0900

    올림픽 선수 문제(객체)

--- day10/src/oop/basic2/Player.java
+++ day10/src/oop/basic2/Player.java
@@ -1,6 +1,10 @@
 package oop.basic2;
 
-public class data {
+//선수(Player) 
+//= 이름(name : String) + 종목(event : String) + 금(gold : int) + 은(silver : int) + 동(bronze : int)
+public class Player {
+	
+	//멤버변수
 	String name;
 	String sport;
 	int gold;
--- day10/src/oop/basic2/Test01.java
+++ day10/src/oop/basic2/Test01.java
@@ -2,15 +2,47 @@ package oop.basic2;
 
 public class Test01 {
 	public static void main(String[] args) {
-		//
-		data a = new data();
+		//객체(선수) 생성
+		Player a = new Player();
+		Player b = new Player();
+		Player c = new Player();
+		
+		//초기화
 		a.name = "김연아";
 		a.sport = "피겨스케이팅";
 		a.gold = 2;
+		a.silver = 0;
 		a.bronze = 1;
 		
+		b.name = "이상화";
+		b.sport = "스피드스케이팅";
+		b.gold = 1;
+		b.silver = 2;
+		b.bronze = 1;
+		
+		c.name = "윤성빈";
+		c.sport = "스켈레톤";
+		c.gold = 1;
+		c.silver = 0;
+		c.bronze = 1;
+		
+		//출력
 		System.out.println(a.name);
-
+		System.out.println(a.sport);
+		System.out.println(a.gold);
+		System.out.println(a.silver);
+		System.out.println(a.bronze);
+		
+		System.out.println(b.name);
+		System.out.println(b.sport);
+		System.out.println(b.gold);
+		System.out.println(b.silver);
+		System.out.println(b.bronze);
+		
+		System.out.println(c.name);
+		System.out.println(c.sport);
+		System.out.println(c.gold);
+		System.out.println(c.silver);
+		System.out.println(c.bronze);
 	}
-
 }
