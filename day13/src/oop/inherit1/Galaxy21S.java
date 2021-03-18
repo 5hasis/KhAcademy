83b13f39caf79c57000cae66e7d09bdb7588e481
하영 <@LAPTOP-0AS9OEBG>
2021-03-18 14:15:45 +0900

    상속 개념 예제
    클래스 사이에 중복이 넘많

---
+++ day13/src/oop/inherit1/Galaxy21S.java
@@ -0,0 +1,23 @@
+package oop.inherit1;
+
+public class Galaxy21S {
+	// 멤버 변수
+	private String number;
+
+	// 멤버 메소드
+	public void call() {
+		System.out.println("전화 기능");
+	}
+
+	public void sms() {
+		System.out.println("문자 기능");
+	}
+
+	public void camera() {
+		System.out.println("카메라 기능");
+	}
+
+	public void bixby() {
+		System.out.println("빅스비 기능");
+	}
+}
---
+++ day13/src/oop/inherit1/IPhone12.java
@@ -0,0 +1,20 @@
+package oop.inherit1;
+
+public class IPhone12 {
+	//멤버 변수
+	private String number;
+	
+	//멤버 메소드
+	public void call() {
+		System.out.println("전화 기능");
+	}
+	public void sms() {
+		System.out.println("문자 기능");
+	}
+	public void camera() {
+		System.out.println("카메라 기능");
+	}
+	public void siri() {
+		System.out.println("시리 기능");
+	}
+}
