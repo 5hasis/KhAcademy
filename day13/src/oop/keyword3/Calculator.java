6d6c643b0073096d628660b3e1dc5f96a0a2d367
하영 <@LAPTOP-0AS9OEBG>
2021-03-18 10:21:01 +0900

    static의 필요성2

---
+++ day13/src/oop/keyword3/Calculator.java
@@ -0,0 +1,14 @@
+package oop.keyword3;
+
+//계산기 : 사칙연산 도우미
+public class Calculator {
+
+	//계산기능
+	public int plus(int a, int b) {
+		return a + b;
+	}
+	public int minus(int a, int b) {
+		return b - a;
+	}
+	
+}
---
+++ day13/src/oop/keyword3/Test01.java
@@ -0,0 +1,14 @@
+package oop.keyword3;
+
+public class Test01 {
+	public static void main(String[] args) {
+		
+		Calculator c = new Calculator();
+		
+		
+		System.out.println(c.plus(10,20));
+		System.out.println(c.minus(10,20));
+		
+		System.out.println(c.minus(30, 20));
+	}
+}
