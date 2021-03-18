3daf781dc192bb73b6b8a2208a120d2cb836f01f
하영 <@LAPTOP-0AS9OEBG>
2021-03-18 10:20:10 +0900

    static의 사용

---
+++ day13/src/oop/keyword4/Calculator.java
@@ -0,0 +1,18 @@
+package oop.keyword4;
+
+//계산기 : 사칙연산 도우미
+public class Calculator {
+
+	//계산기능
+	//static : 정적
+	//- 객체 정보에 포함되지 않음
+	//= 탈 객체지향 키워드
+	//- 클래스에 고정시켜 편하게 사용하려는 목적 -> 객체 생성하지 않고 쓰겟당!
+	public static int plus(int a, int b) {
+		return a + b;
+	}
+	public static int minus(int a, int b) {
+		return b - a;
+	}
+	
+}
---
+++ day13/src/oop/keyword4/Test01.java
@@ -0,0 +1,12 @@
+package oop.keyword4;
+
+public class Test01 {
+	public static void main(String[] args) {
+		
+		//일회용 계산 메소드 등에 적합한 형태
+		System.out.println(Calculator.plus(10,20));
+		System.out.println(Calculator.minus(10,20));
+		
+		System.out.println(Calculator.minus(30, 20));
+	}
+}
