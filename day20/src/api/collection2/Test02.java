164edf386ad985f319b3fb369e34f017e119525a
하영 <@LAPTOP-0AS9OEBG>
2021-03-29 14:59:05 +0900

    로또 추첨(Set)

---
+++ day20/src/api/collection2/Test02.java
@@ -0,0 +1,16 @@
+package api.collection2;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test02 {
+	public static void main(String[] args) {
+		
+		Set<Integer> lotto = new TreeSet<>();
+		Random r = new Random();
+		
+		int n = r.nextInt(45) + 1;
+		
+	}
+}
