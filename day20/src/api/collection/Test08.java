55fd60dd88cacf87f9893d2d02e379dd6115e1bc
하영 <@LAPTOP-0AS9OEBG>
2021-03-29 14:31:51 +0900

    List종류별 특징

---
+++ day20/src/api/collection/Test08.java
@@ -0,0 +1,13 @@
+package api.collection;
+
+public class Test08 {
+//  thread - safe : 줄을 서시오~ => 느림 & 안정적
+
+//	Q : List에는 종류가 여러 가지가 있는데... 어떤게 좋은가?
+//	= ArrayList 와 LinkedList를 비교하는 질문이 많이 나온다.
+
+//	ArrayList : 조회가 빠름
+//	LinkedList : 구조변경이 빠름(추가/삭제)
+//	CopyOnWriteArrayList : ArrayList의 Thread-safe 버전
+
+}
