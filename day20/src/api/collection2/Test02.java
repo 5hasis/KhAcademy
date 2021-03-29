ac5417aee8bc48b0bfc97b72e6d0d9d82a1373ad
하영 <@LAPTOP-0AS9OEBG>
2021-03-29 14:59:39 +0900

    로또 추첨(Set)

--- day20/src/api/collection2/Test02.java
+++ day20/src/api/collection2/Test02.java
@@ -6,11 +6,20 @@ import java.util.TreeSet;
 
 public class Test02 {
 	public static void main(String[] args) {
-		
+		// 저장소 생성
 		Set<Integer> lotto = new TreeSet<>();
+
+		// 데이터 추가
 		Random r = new Random();
-		
-		int n = r.nextInt(45) + 1;
-		
+
+		while (lotto.size() < 6) {
+			int num = r.nextInt(45) + 1;
+			lotto.add(num);
+
+		}
+
+		// 출력
+		System.out.println(lotto);
+
 	}
 }
