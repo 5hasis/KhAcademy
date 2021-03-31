c3c1990c20016b8236abf5c9dbd0caed4577d3db
하영 <@LAPTOP-0AS9OEBG>
2021-03-31 11:27:04 +0900

    로또번호 입출력

--- day22/src/api/io/multi/Test03_1.java
+++ day22/src/api/io/multi/Test03_1.java
@@ -6,15 +6,32 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class Test03_1 {
 	public static void main(String[] args) throws IOException {
-		// 로또번호 6개 추첨해서 lotto.kh라는 파일에 저장
+//		로또 번호 6개 추첨 후 kh/lotto.kh에 저장
+		List<Integer> base = new ArrayList<>();
+		for(int i=1; i <=45; i++) {
+			base.add(i);
+		}
+		Collections.shuffle(base);
 		
+		List<Integer> lotto = base.subList(0, 6);
+		Collections.sort(lotto);
+		
+//		파일 출력
+		File target = new File("kh", "lotto.kh");
+		FileOutputStream out = new FileOutputStream(target);
+		BufferedOutputStream buffer = new BufferedOutputStream(out);
+		DataOutputStream data = new DataOutputStream(buffer);
+		
+		for(int number : lotto) {
+			data.writeInt(number);
+		}
+		
+//		data.flush();
+		data.close();
 	}
-
 }
