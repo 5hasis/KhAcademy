cbff639622344408e824e5d6b75b22f91ea0ffb3
5hasis <dhgkdud4002@naver.com>
2021-06-09 20:00:13 +0900

    Runtime 클래스

--- day18/src/api/lang/etc/Test01.java
+++ day18/src/api/lang/etc/Test01.java
@@ -5,7 +5,7 @@ public class Test01 {
 		//Math 클래스 : 수학 계산 클래스
 		//= 생성자가 잠겨 있다(private)
 		//= new를 쓸 수 없다
-		//= 아무데서나 편하게 불러오도록 전부다 static 등록
+		//= 아무데서나 편하게 불러오도록 전부다 static 등록.
 
 		//Math m = new Math();//안됨
 		
