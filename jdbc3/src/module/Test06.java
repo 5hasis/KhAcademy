16bd572d68d8969ee18e8688881b7706c1c45441
5hasis <dhgkdud4002@naver.com>
2021-06-09 19:05:23 +0900

    DAO, DTO 이용 데이터베이스 조회

--- jdbc3/src/module/Test06.java
+++ jdbc3/src/module/Test06.java
@@ -7,7 +7,8 @@ import beans.AcademyDto;
 
 public class Test06 {
 	public static void main(String[] args) throws Exception {
-//		Q. MemberDao, MemberDto를 활용한 데이터베이스 조회(Read)작업 수행하기
+//		Q : AcademyDao, AcademyDto를 활용한 데이터베이스 조회(Read) 작업을 수행할 수 있는가?
+//		= 데이터베이스 조회를 통해서 얻고 싶은 데이터는 ??? 형태이다.
 		
 //		준비 데이터 없음.
 		
