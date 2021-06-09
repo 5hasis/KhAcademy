e6f30f07c564c9e2082c601528d9890cfae5bbe8
5hasis <dhgkdud4002@naver.com>
2021-06-09 21:06:41 +0900

    랜덤 예제

---
+++ day14/src/oop/poly2/Test04.java
@@ -0,0 +1,36 @@
+package oop.poly2;
+
+import java.util.Random;
+
+public class Test04 {
+	public static void main(String[] args) {
+//		1개의 전화기를 랜덤으로 생성
+		
+//		약속 : 갤럭시21s(0) , 갤럭시노트10(1) , 아이폰11(2) , 아이폰12(3)
+//		= 0부터 3까지 = 0부터 4개
+		
+		Random r = new Random();
+		int choice = r.nextInt(4) + 0;
+		System.out.println("choice = " + choice);
+		
+//		Phone p = 4개중 1개;
+		Phone p;
+		if(choice == Phone.GALAXY21S) {
+			p = new Galaxy21S("010-1212-3434", "골드");
+		}
+		else if(choice == Phone.GALAXYNOTE10) {
+			p = new GalaxyNote10("010-1212-3434", "골드");
+		}
+		else if(choice == Phone.IPHONE11) {
+			p = new IPhone11("010-1212-3434", "화이트");
+		}
+		else {
+			p = new IPhone12("010-1212-3434", "화이트");
+		}
+		
+		p.call();
+		p.sms();
+		
+	}
+}
+
