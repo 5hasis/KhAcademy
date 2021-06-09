b990753b98cba7a4cc9013fa7118c235a0b1b5fb
5hasis <dhgkdud4002@naver.com>
2021-06-09 19:07:02 +0900

    검색 예제

--- jdbc3/src/module/Test08.java
+++ jdbc3/src/module/Test08.java
@@ -8,11 +8,13 @@ import beans.AcademyDto;
 public class Test08 {
 	public static void main(String[] args) throws Exception {
 
+		
 		String keyword = "당";
 
 		AcademyDao academyDao = new AcademyDao();
 		List<AcademyDto> list = academyDao.searchList(keyword);
 
+		//데이터베이스 검색
 		if (list.isEmpty()) { //list.size == 0
 			System.out.println("검색 결과가 존재하지 않습니다.");
 		} 
