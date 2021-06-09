4d2e4450aaa3db7783391024ca34039fb815e8fd
5hasis <dhgkdud4002@naver.com>
2021-06-09 20:57:42 +0900

    동적 타입 생성

--- day14/src/oop/inherit9/Test02.java
+++ day14/src/oop/inherit9/Test02.java
@@ -2,6 +2,7 @@ package oop.inherit9;
 
 public class Test02 {
 	public static void main(String[] args) {
+		
 		//상속 구조의 응용 ----> 다형성
 		//상위 형태의 리모컨(참조변수)로 하위 형태의 객체를 관리할 수 있다.
 		File a = new MP3("test.mp3");
