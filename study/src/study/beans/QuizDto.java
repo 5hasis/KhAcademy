34dee6204635461043e4f0f54f41f9504d86d35e
5hasis <dhgkdud4002@naver.com>
2021-06-11 00:41:54 +0900

    문항 등록 시스템 개발

---
+++ study/src/study/beans/QuizDto.java
@@ -0,0 +1,29 @@
+package study.beans;
+
+public class QuizDto {
+	
+	private String quizContent;
+	private int quizNo;
+	
+	public QuizDto() {
+		super();
+	}
+
+	public String getQuizContent() {
+		return quizContent;
+	}
+
+	public void setQuizContent(String quizContent) {
+		this.quizContent = quizContent;
+	}
+
+	public int getQuizNo() {
+		return quizNo;
+	}
+
+	public void setQuizNo(int quizNo) {
+		this.quizNo = quizNo;
+	}
+	
+
+}
