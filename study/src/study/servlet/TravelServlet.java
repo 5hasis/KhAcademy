c9bbc5862ffaf186d2aea1c7b87e1bbbc523f60d
5hasis <dhgkdud4002@naver.com>
2021-06-11 00:54:28 +0900

    여행비 계산 서블릿

--- study/src/study/servlet/TravelServlet.java
+++ study/src/study/servlet/TravelServlet.java
@@ -10,31 +10,29 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/travel.kh")
 public class TravelServlet extends HttpServlet{
-
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+//		준비 : 파라미터 수신 코드
 		int people = Integer.parseInt(req.getParameter("people"));
 		int month = Integer.parseInt(req.getParameter("month"));
 		int days = Integer.parseInt(req.getParameter("days"));
 		
-		int pay = people * days * 100000;
-		int sale = 1;
+//		계산
+//		1. 할인율 계산
+//		int rate = 10 or 5 or 15 or 20;
+		int rate;
+		if(month / 3 == 1) 			rate = 10;
+		else if(month / 3 == 2) 	rate = 5;
+		else if(month / 3 == 3) 	rate = 15;
+		else 									rate = 20;
 		
-		if(month >=3 && month <= 5) {
-			sale = 10;
-		}
-		else if(month >= 6 && month < 8) {
-			sale = 5;
-		}
-		else if(month >= 9 && month < 11) {
-			sale = 15;
-		}
-		else if(month == 12 || month == 1 || month == 2) {
-			sale = 20;
-		}
+//		2. 요금계산
+//		int price = days * 10만원 * people * 할인율;
+		int price = days * 100000 * people  * (100 - rate) / 100;
 		
-		pay = pay * (100 - sale) / 100;
-		resp.getWriter().println(pay);
+//		출력 : 응답객체를 이용한 출력
+		resp.getWriter().println(price);
+		
 	}
-	
 }
