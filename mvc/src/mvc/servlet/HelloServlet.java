050170c007b612fb552b850fb5f4585b5fe57585
5hasis <dhgkdud4002@naver.com>
2021-06-11 00:35:18 +0900

    HelloServlet + jsp

---
+++ mvc/src/mvc/servlet/HelloServlet.java
@@ -0,0 +1,29 @@
+package mvc.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/hello.kh")
+public class HelloServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			
+			//hello.jsp로 포워드하는 코드
+			RequestDispatcher dispatcher = 
+					req.getRequestDispatcher("/WEB-INF/hello.jsp");
+			dispatcher.forward(req, resp);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+}
+
