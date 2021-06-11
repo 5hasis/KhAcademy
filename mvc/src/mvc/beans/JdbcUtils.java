14eeb866e7be7fba0af7b0845209a82d193e2816
5hasis <dhgkdud4002@naver.com>
2021-06-11 14:36:52 +0900

    JdbcUtils 수정

--- mvc/src/mvc/beans/JdbcUtils.java
+++ mvc/src/mvc/beans/JdbcUtils.java
@@ -13,15 +13,18 @@ public class JdbcUtils {
 	private static DataSource ds;
 	static {
 		try {
+			//1. 자원 탐색 도구를 생성한다(InitialContext)
 			Context ctx = new InitialContext();
-			ds = (DataSource)ctx.lookup("java:comp/env/jdbc/oracle");
+			//2. 경로를 식으로 알려주고 탐색을 명한다.
+			ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/oracle");
 		}
 		catch(Exception e) {
+			System.err.println("데이터베이스 연결 실패");
 			e.printStackTrace();
 		}
 	}
-
-	public static Connection getConnection() throws Exception {
+	
+	public static Connection getConnection() throws Exception{
 		return ds.getConnection();
 	}
 	
