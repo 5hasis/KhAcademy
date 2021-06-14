3a05c1f1a13b2d5f22137aebb481f00597f8302a
5hasis <dhgkdud4002@naver.com>
2021-06-14 17:15:56 +0900

    변화된 방식의 컨트롤러 구현

---
+++ spring03-controller/src/main/java/com/kh/spring03/HomeController.java
@@ -0,0 +1,21 @@
+package com.kh.spring03;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller//이 클래스가 "컨트롤러" 계층임을 명시하는 어노테이션
+public class HomeController{
+	
+	//아래 메소드는 "/" 라는 요청에 대한 처리 내용임을 명시하는 어노테이션
+	@RequestMapping("/")
+	public String home() {
+		return "/WEB-INF/views/home.jsp";
+	}
+	
+	@RequestMapping("/dice")
+	public String dice() {
+		return "/WEB-INF/views/dice.jsp";
+	}
+	
+}
+
