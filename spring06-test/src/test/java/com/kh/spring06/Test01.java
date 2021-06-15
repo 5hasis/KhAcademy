68867f307be52ce749f6971120481f8cfd4faad5
5hasis <dhgkdud4002@naver.com>
2021-06-15 15:49:11 +0900

    테스트 코드 추가

---
+++ spring06-test/src/test/java/com/kh/spring06/Test01.java
@@ -0,0 +1,14 @@
+package com.kh.spring06;
+
+import org.junit.Test;
+
+public class Test01 {
+	
+	//테스트 메소드 구현
+	//형태 : public void 이름(){ 코드 }
+	@Test
+	public void test() {
+		System.out.println("테스트 코드");
+	}
+	
+}
