6f03607c312512d4fcb44ebe489c8a7862462699
5hasis <dhgkdud4002@naver.com>
2021-06-17 11:23:06 +0900

    비밀번호 변경 테스트

--- spring-home/src/test/java/com/kh/home/Test04_MemberChangePw.java
+++ spring-home/src/test/java/com/kh/home/Test04_MemberChangePw.java
@@ -1,5 +1,9 @@
 package com.kh.home;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +19,32 @@ import org.springframework.test.context.web.WebAppConfiguration;
 })
 @WebAppConfiguration
 public class Test04_MemberChangePw {
+	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	private int memberNo;
+	private String currentPassword;
+	private String newPassword;
+	
+	@Before 
+	public void before() {
+		memberNo = 1;
+		currentPassword = "test0001";
+		newPassword = "test0001";
+	}
 	
 	@Test
 	public void test() {
 		String sql = "update member set member_pw = ? where member_no = ? and member_pw = ?";
-		Object[] param = {"test0001", 1, "test0001"};
-		jdbcTemplate.update(sql, param);
+		Object[] param = {newPassword, memberNo, currentPassword};
+		int count = jdbcTemplate.update(sql, param);
 		
+		assertEquals(1,count);
+	}
+	
+	@After 
+	public void after() {
+		//롤백 등의 작업
 	}
 }
