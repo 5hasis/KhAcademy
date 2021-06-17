bb56412e97eb277ef0f665a9eb97e41f114c1a83
5hasis <dhgkdud4002@naver.com>
2021-06-17 14:39:14 +0900

    롬복 적용

--- spring-home/src/main/java/com/kh/home/entity/MemberDto.java
+++ spring-home/src/main/java/com/kh/home/entity/MemberDto.java
@@ -2,8 +2,11 @@ package com.kh.home.entity;
 
 import java.sql.Date;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor 
 public class MemberDto {
-	
 	private int memberNo;
 	private String memberId;
 	private String memberPw;
@@ -14,69 +17,4 @@ public class MemberDto {
 	private String memberEmail;
 	private int memberPoint;
 	private String memberGrade;
-	
-	public MemberDto() {
-		super();
-	}
-	public int getMemberNo() {
-		return memberNo;
-	}
-	public void setMemberNo(int memberNo) {
-		this.memberNo = memberNo;
-	}
-	public String getMemberId() {
-		return memberId;
-	}
-	public void setMemberId(String memberId) {
-		this.memberId = memberId;
-	}
-	public String getMemberPw() {
-		return memberPw;
-	}
-	public void setMemberPw(String memberPw) {
-		this.memberPw = memberPw;
-	}
-	public String getMemberNick() {
-		return memberNick;
-	}
-	public void setMemberNick(String memberNick) {
-		this.memberNick = memberNick;
-	}
-	public String getMemberBirth() {
-		return memberBirth;
-	}
-	public void setMemberBirth(String memberBirth) {
-		this.memberBirth = memberBirth;
-	}
-	public String getMemberPhone() {
-		return memberPhone;
-	}
-	public void setMemberPhone(String memberPhone) {
-		this.memberPhone = memberPhone;
-	}
-	public Date getMemberJoin() {
-		return memberJoin;
-	}
-	public void setMemberJoin(Date memberJoin) {
-		this.memberJoin = memberJoin;
-	}
-	public String getMemberEmail() {
-		return memberEmail;
-	}
-	public void setMemberEmail(String memberEmail) {
-		this.memberEmail = memberEmail;
-	}
-	public int getMemberPoint() {
-		return memberPoint;
-	}
-	public void setMemberPoint(int memberPoint) {
-		this.memberPoint = memberPoint;
-	}
-	public String getMemberGrade() {
-		return memberGrade;
-	}
-	public void setMemberGrade(String memberGrade) {
-		this.memberGrade = memberGrade;
-	}
-	
 }
