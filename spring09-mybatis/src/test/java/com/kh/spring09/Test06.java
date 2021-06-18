43304502828d0676471f419457259767cc76196a
5hasis <dhgkdud4002@naver.com>
2021-06-18 15:35:45 +0900

    항목 및 키워드 검색

---
+++ spring09-mybatis/src/test/java/com/kh/spring09/Test06.java
@@ -0,0 +1,49 @@
+package com.kh.spring09;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.kh.spring09.entity.ItemDto;
+
+public class Test06 {
+	private SqlSession sqlSession;
+	
+	@Before
+	public void before() throws IOException {
+		InputStream in = Resources.getResourceAsStream("mybatis/mybatis-config.xml");
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+		sqlSession = factory.openSession(false);
+	}
+	
+	@Test
+	public void test() {
+		//원하는 항목과 원하는 값을 이용하여 검색을 수행
+		//ex: type=item_name, keyword=피자10판이라면 item_name=피자10판인 항목을 조회
+		Map<String, Object> map = new HashMap<>();
+		map.put("type","item_name");
+		map.put("keyword","피자10판");
+		
+		List<ItemDto> list = sqlSession.selectList("item.select3",map);
+		for(ItemDto itemDto : list) {
+			System.out.println(itemDto);
+		}
+
+	}
+	
+	@After
+	public void after() {
+//		sqlSession.commit();
+		sqlSession.rollback();
+	}
+}
