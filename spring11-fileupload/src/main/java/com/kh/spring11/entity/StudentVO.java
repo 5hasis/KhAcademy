eca40d8700ad71a7922b1aec2261de72523e996a
5hasis <dhgkdud4002@naver.com>
2021-06-28 10:36:45 +0900

    VO설명 주석 추가

--- spring11-fileupload/src/main/java/com/kh/spring11/entity/StudentVO.java
+++ spring11-fileupload/src/main/java/com/kh/spring11/entity/StudentVO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class StudentVO {
 	//Dto는 데이터베이스와 똑같아야함
-	//VO가 더 큰개념 -> DB는 원시형데이터만 저장하는거예요
+	//VO가 더 큰개념 -> DB는 원시형데이터만 저장하는거예요.
 	//Value Object
 	private String studentName;
 	private String studentEmail;
