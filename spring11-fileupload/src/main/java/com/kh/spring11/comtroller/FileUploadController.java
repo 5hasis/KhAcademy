b49206f499a61db4db53d86cf0e42061166db12b
5hasis <dhgkdud4002@naver.com>
2021-06-28 10:30:06 +0900

    이메일 발송 서비스 구현 및 테스트

--- spring11-fileupload/src/main/java/com/kh/spring11/comtroller/FileUploadController.java
+++ spring11-fileupload/src/main/java/com/kh/spring11/comtroller/FileUploadController.java
@@ -27,19 +27,18 @@ public class FileUploadController {
 	 * Spring에서는 파일을 MultipartFile 이라는 형태로 수신. 
 	 * = MultipartResolver가 등록되어있어야 수신 가능
 	 * = cos.jar와는 다르게 먼저 저장을 하지 않고 명령을 통해 저장
-	 * 
 	 **/
 	
 	@PostMapping("/upload1")
-	public String upload1(@RequestParam MultipartFile f) throws IllegalStateException, IOException {
+	public String upload1(@RequestParam(required = false) MultipartFile f) throws IllegalStateException, IOException {
 		System.out.println("f = " + f);
 		System.out.println("파일없어? : " + f.isEmpty());
 		
 		if(!f.isEmpty()) {
-			System.out.println("name = " + f.getName());
-			System.out.println("originalFilename = " + f.getOriginalFilename());
-			System.out.println("size = " + f.getSize());
-			System.out.println("contentType = " + f.getContentType());
+			System.out.println("name = " + f.getName()); //f
+			System.out.println("originalFilename = " + f.getOriginalFilename()); //dog.png
+			System.out.println("size = " + f.getSize()); //345197
+			System.out.println("contentType = " + f.getContentType()); //image/png
 			
 			File dir = new File("E:/Kh/upload");
 			File target = new File(dir, f.getOriginalFilename());
@@ -54,12 +53,12 @@ public class FileUploadController {
 	 **/
 	@PostMapping("/upload2")
 	//public String upload2(@RequestParam MultipartFile[] f) {
-	public String upload2(@RequestParam List<MultipartFile> f) {
+	public String upload2(@RequestParam List<MultipartFile> f) { //없어도 한개는 비어있는 상태로 들어옴
 		
 		if(!f.get(0).isEmpty()) {
 			for(MultipartFile mf : f) {
 				System.out.println("파일 발견!");
-				System.out.println("name = " + mf.getName());
+				System.out.println("name = " + mf.getName()); //f
 				System.out.println("originalFilename = " + mf.getOriginalFilename());
 				System.out.println("size = " + mf.getSize());
 				System.out.println("contentType = " + mf.getContentType());
@@ -76,7 +75,7 @@ public class FileUploadController {
 			@RequestParam MultipartFile studentProfile) {
 		
 		System.out.println("이름 = " + studentName);
-		System.out.println("name = " + studentProfile.getName());
+		System.out.println("name = " + studentProfile.getName()); //studentProfile
 		System.out.println("originalFilename = " + studentProfile.getOriginalFilename());
 		System.out.println("size = " + studentProfile.getSize());
 		System.out.println("contentType = " + studentProfile.getContentType());
@@ -88,8 +87,20 @@ public class FileUploadController {
 	private StudentService studentService;
 
 	@PostMapping("/upload4")
-	public String upload3(@ModelAttribute StudentVO studentVO) throws IllegalStateException, IOException {
+	public String upload4(@ModelAttribute StudentVO studentVO) throws IllegalStateException, IOException {
 		studentService.studentRegistration(studentVO);
+		return "redirect:upload";
+	}
+	
+	//--------------------(과정)-----------------------
+	@PostMapping("/upload4")
+	public String upload5(@ModelAttribute StudentVO studentVO) {
+		//1-4. <학생 등록 트랜잭션(서비스)>
+		//	1.학생 번호를 획득
+		//	2.학생 등록
+		//	3.학생 프로필 파일 등록
+		//	= 파일 이름을 학생 번호로 설정(학생당 파일 하나이기 때문에)
+		//	4.학생 프로필 정보 등록
 		return "redirect:upload";
 	}
 }
--- spring11-fileupload/src/main/java/com/kh/spring11/entity/StudentVO.java
+++ spring11-fileupload/src/main/java/com/kh/spring11/entity/StudentVO.java
@@ -10,8 +10,8 @@ import lombok.NoArgsConstructor;
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class StudentVO {
 	//Dto는 데이터베이스와 똑같아야함
-	//VO가 더 큰개념
-	
+	//VO가 더 큰개념 -> DB는 원시형데이터만 저장하는거예요
+	//Value Object
 	private String studentName;
 	private String studentEmail;
 	private MultipartFile studentProfile;
