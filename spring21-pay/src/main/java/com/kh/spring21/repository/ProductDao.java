7705435edd3e5a7534041499b113f7272696b6d9
5hasis <dhgkdud4002@naver.com>
2021-07-06 21:53:19 +0900

    상품 목록 추가

---
+++ spring21-pay/src/main/java/com/kh/spring21/repository/ProductDao.java
@@ -0,0 +1,13 @@
+package com.kh.spring21.repository;
+
+import java.util.List;
+
+import com.kh.spring21.entity.ProductDto;
+
+public interface ProductDao {
+
+	//목록 기능
+	List<ProductDto> list();
+	//상세조회 기능
+	ProductDto get(int no);
+}
