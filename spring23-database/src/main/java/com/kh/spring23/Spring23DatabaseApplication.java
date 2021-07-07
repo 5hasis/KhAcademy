a4f3d99cc1695a3b3e4d20cbf882160133cae21e
5hasis <dhgkdud4002@naver.com>
2021-07-07 21:50:35 +0900

    spring23 업로드

---
+++ spring23-database/src/main/java/com/kh/spring23/Spring23DatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring23DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring23DatabaseApplication.class, args);
+	}
+
+}
